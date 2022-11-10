0954c3ed5ad8e6073dc4ecceea765aa9960f4bfa
xiaoyuanxun <53613219+xiaoyuanxun@users.noreply.github.com>
2022-11-10 14:45:03 +0800

    update main

--- README.md
+++ README.md
@@ -1,64 +1,7 @@
 # adopt-a-person
 
-## 功能：
+- 领养一个人
 
-- 产品展示
-    ```
-    // 有机蔬果结构体
-    struct Organic {
-        index: Nat; // 第几种套餐
-        varieties: [Text]; // 品种
-        functions: [Text]; // 对应的功效
-        amount: Nat; // 每个品种对应多少kg
-        img_url: [Text]; // 图片存储的url
-        var price: Nat; // 价格
-    }
-    ```
+## 合约功能接口
 
-    合约： 查询所有的有机蔬果; 添加/删除相关套餐
-    
-    前端： 展示
-
-    后端：合约调用；存图片
-
-
--  购买套餐：
-    ```
-    struct BuyArgs {
-        organic_index: Nat; // 第几种套餐
-        amount: Nat; // 购买几份
-        time: Int;
-    }
-    // 返回给后端
-    struct BuyOrder {
-        buyer: Address;
-        buy_arg: BuyArgs;
-        time: Int;
-    }
-    ```
-    前端：相应页面；
-    
-    后端： 调合约代币的转账；提供BuyArgs调合约的买函数会返回BuyOrder；
-
-- 人无改善退款：
-    ```
-    struct Refund {
-        buy_order: BuyOrder;
-        user_body_data: BodyData;
-        reference_body_data: BodyData;
-    }
-    struct BodyData {
-        heart_rate: Nat;
-        blood_sugar_concentration: Nat;
-        blood_oxygen_saturation: Nat;
-    }
-    
-    ```
-    合约：退款函数
-    
-    前端：相应页面
-
-    后端：提供Refund参数调退款函数
-
-- ERC20代币
-       
+```
--- contracts/main.sol
+++ contracts/main.sol
@@ -5,24 +5,79 @@ contract main {
     struct Organic {
         string variety;
         string benefit;
-        uint amount;
         string img_url;
-    }
-    struct Combo {
-        uint index;
-        Organic _organic;
-        uint price;
+        uint amount;
     }
 
+    struct ReferenceBodyData {
+        (uint, uint) blood_pressure;
+        (uint, uint) heart_rate;
+        (uint, uint) blood_oxygen;
+    }
+
+    struct Combo {
+        address seller;
+        uint index;
+        uint price;
+        bool on_sale;
+        Organic _organic;
+        ReferenceBodyData Reference_body_data;
+    }
+
+    uint combo_index;
     mapping (uint => Combo) public combos;
 
     function get_combo(uint index) public view returns(Combo memory) {
-        return combos[index];
+
     }
 
-    function add_combo(uint index, Combo memory new_combo) public payable returns(bool) {
-        combos[index] = new_combo;
-        return true;
+    function add_combo(Combo memory new_combo) public payable returns(bool) {
+
     }
+
+    struct Stake {
+        uint buy_tx_id;
+        uint amount;
+    }
+
+    mapping (uint => Stake) public stakeOrders;
+
+    struct BuyArgs {
+        uint combo_index;
+        uint amount;
+    }
+
+    struct BuyOrder {
+        address buyer;
+        uint buy_tx_id;
+        uint time;
+        BuyArgs buy_args;
+    }
+
+    uint buy_tx;
+    mapping (uint => BuyOrder) public buyOrders;
+
+    function buy(BuyArgs memory args) public payable returns BuyOrder {
+
+    }
+
+    struct RefundArgs {
+        uint buy_tx_id;
+    }
+
+    struct RefundOrder {
+        address buyer;
+        address refunder;
+        uint buy_tx_id;
+        uint refund_tx_id;
+    }
+
+    uint refund_tx;
+    mapping (uint => RefundOrder) public refundOrders;
+
+    function refund(RefundArgs args) public payable returns RefundOrder {
+
+    }
+
 
 }
